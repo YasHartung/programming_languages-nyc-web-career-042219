0e013bb706929e000bd64352ff7a7bb6d96b38a6
Yasmine Hartung <y.hartung@gmail.com>
2019-04-19 14:58:06 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+
+  languages.each do |style, language_data|
+    language_data.each do |language_name, data|
+      data.each do |type, type_data|
+        new_hash[language_name] = { type => type_data, style: style}
+      end
+    end
+  end
+new_hash
 end
